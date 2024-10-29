bc72241bfb8d5d1ca28ed7ef762d9a00db3a3671
Saurav Singh Khekhaliya <49152290+sskhekhaliya@users.noreply.github.com>
2024-10-29 23:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (48.75%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        // Using Dutch National Flag Algorithm
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while(mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, mid, low);
+                mid++;
+                low++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}
